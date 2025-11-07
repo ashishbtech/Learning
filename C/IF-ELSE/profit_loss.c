1d9c9a95cc7d78986c2dbf1b8355c49a5eb312db
ashishbtech <ashishbtech.cs@gmail.com>
2025-11-07 21:57:39 +0530

    Update profit_loss.c

--- C/IF-ELSE/profit_loss.c
+++ C/IF-ELSE/profit_loss.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    
+    int sp,cp;
+    printf("Print your Selling price and Cost Price\n");
+    scanf("%d %d", &sp, &cp);
+    if (sp>cp)
+    {
+        printf("Profit\n");
+    }
+    else
+    {
+        printf("Loss\n");
+    }
+
     return 0;
 
 }
