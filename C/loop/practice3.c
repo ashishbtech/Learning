3ac2c595ef77bab92db057b4c398c14f567bcc9d
Ashish Kushwaha <ashishbtech.cs@gmail.com>
2025-11-10 22:04:46 +0530

    Add files via upload

---
+++ C/loop/practice3.c
@@ -0,0 +1,35 @@
+// Write a program in C to display the cube of the number up to an integer.
+// Test Data :
+// Input number of terms : 5
+// Expected Output :
+// Number is : 1 and cube of the 1 is :1
+// Number is : 2 and cube of the 2 is :8
+// Number is : 3 and cube of the 3 is :27
+// Number is : 4 and cube of the 4 is :64
+// Number is : 5 and cube of the 5 is :125
+#include <stdio.h>
+int main()
+{
+    int i, n;
+    printf("Input number of terms :");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        printf("Number is : %d and cube of the %d is :%d\n", i, i, (i * i * i));
+    }
+    return 0;
+}
+
+// solution is below by problem maker
+// #include <stdio.h>  // Include the standard input/output header file.
+
+// void main() {
+//     int i, ctr;  // Declare variables 'i' for loop counter and 'ctr' for user input.
+
+//     printf("Input number of terms : ");  // Print a message to prompt user input.
+//     scanf("%d", &ctr);  // Read the value of 'ctr' from the user.
+
+//     for (i = 1; i <= ctr; i++) {  // Start a for loop to iterate 'ctr' times.
+//         printf("Number is : %d and cube of the %d is :%d \n", i, i, (i * i * i));  // Print the number, its cube, and message.
+//     }
+// }
